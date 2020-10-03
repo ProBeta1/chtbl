aad961bae51af2465c098130f2d81f90564f9b63
Darren <darrenzhang200027@gmail.com>
2020-10-03 17:22:56 -0400

    added chat

--- src/screens/Groups/Groups.js
+++ src/screens/Groups/Groups.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import styles from './styles';
 import { firebase } from '../../firebase/config'
+import styles from './styles';
+
 
 const Groups = () => {
     return (
-        <View>
-            <Text>Groups</Text>
+        <View style={styles.container}>
+            <Text style={{
+                fontFamily: 'Montserrat',
+                fontStyle: 'normal',
+                fontWeight: '600',
+                fontSize: '14px',
+                lineHeight: '17px'
+            }}>The focus this week is </Text>
+
+            <View sty>
+                <Text>Hiring bias</Text>
+            </View>
+
+            <Text>Watch the introductory video</Text>
+
         </View>
     )
 }
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -2,14 +2,67 @@ import React, { useEffect, useState } from 'react'
 import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import styles from './styles';
 import { firebase } from '../../firebase/config'
+import { Searchbar } from 'react-native-paper';
+import Img1 from "../../pics/user.png"
+import Img2 from "./images/img2.png"
+import { Image } from 'react-native'
+import user from '../../pics/user.png';
+import { WebView } from 'react-native';
+
 
 export default function HomeScreen(props) {
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+    const onChangeSearch = query => setSearchQuery(query);
 
     return (
         <View style={styles.container}>
-            <Text>
-                Home yes!
+            <Searchbar
+                placeholder="Search"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+            />
+            <Text style={{
+                fontFamily: 'Montserrat',
+                fontStyle: 'normal',
+                fontWeight: '600',
+                fontSize: '18px',
+                lineHeight: '22px',
+                margin: '30px'
+            }}>
+                Recommended videos
+                Very confused why the image imports below aren't working
             </Text>
+
+            {/* <WebView
+                style={{
+                    flex: 1,
+                    width: '200px',
+                    height: '200px'
+                }}
+                javaScriptEnabled={true}
+                source={{ uri: 'https://www.youtube.com/watch?v=QCFb4BiDDcE&ab_channel=TEDxTalks' }}
+            /> */}
+
+            {/* ONLY WORKS ON MOBILE NOT WEB */}
+            {/* <YouTube
+                videoId="ZZ5LpwO-An4" // The YouTube video ID
+                play // control playback of video with true/false
+                fullscreen // control whether the video should play in fullscreen or inline
+                loop // control whether the video should loop when ended
+                onReady={e => this.setState({ isReady: true })}
+                onChangeState={e => this.setState({ status: e.state })}
+                onChangeQuality={e => this.setState({ quality: e.quality })}
+                onError={e => this.setState({ error: e.error })}
+                style={{ alignSelf: 'stretch', height: 300 }}
+            /> */}
+
+            <Image
+                source={user}
+            />
+            <Image
+                source={require('./images/img2.png')}
+            />
 
         </View>
     )
